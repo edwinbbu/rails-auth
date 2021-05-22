aacac4fe81f2812cd7b589d1e80194b542cb3632
Edwin Babu <edwinbbu@gmail.com>
2021-05-22 23:10:51 +0530

    Foreign key added to task table

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_110627) do
+ActiveRecord::Schema.define(version: 2021_05_22_170131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_05_22_110627) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_05_22_110627) do
     t.string "authentication_token"
   end
 
+  add_foreign_key "tasks", "users", on_delete: :cascade
 end
